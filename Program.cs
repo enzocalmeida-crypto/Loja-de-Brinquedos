427ff8e5d59a17803981b4b549e038c5f5899c58
ENZO CAETANO ALMEIDA <enzo.almeida@portalsesisp.org.br>
2026-06-18 14:36:24 -0300

    aaaaaa

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* codigo é gerado quando o produto é cadastrado, ELE NÃO É MOSTRADO NO TERMINAL, o numero do codigo é de acordo com a ordem do cadastro ex:
+primeiro produto cadastrado - codigo 1
+Segundo produto cadastrado - codigo 2 
+e assim por diante
+*/
+
+using System;
 
 public class Produto
 {
@@ -16,140 +22,218 @@ class Program
     static Produto[] produtos = new Produto[MAX_PRODUTOS];
     static int quantidadeProdutos = 0;
     static int proximoCodigo = 1;
-
+  
     static void Main()
     {
-        int opcao;
+        int opcao = 0;
 
-        /* 
-        * Escreva o menu utilizando a estrututura de repetição do-while 
-        *   - O usuário deverá entrar com uma opção entre:
-        *       -- Cadastrar Produto
-        *       -- Frente de Caixa
-        *       -- Consultar Estoque
-        *       -- Entrada de Produtos
-        *       -- Listagem de Produtos
-        *       -- Sair
-        *
-        * Utilize a estrutura switch-case para validar a entrada do usuário
-        */
+        do
+        {
+            Console.Clear();
+ 
+            Console.WriteLine("=================================");
+            Console.WriteLine("Loja de Brinquedos");
+            Console.WriteLine("=================================");
+            Console.WriteLine("1 - Cadastrar Produto");
+            Console.WriteLine("2 - Frente de Caixa");
+            Console.WriteLine("3 - Consultar Estoque");
+            Console.WriteLine("4 - Entrada de Produtos");
+            Console.WriteLine("5 - Listagem de Produtos");
+            Console.WriteLine("6 - Sair");
+            Console.Write("Escolha uma opção: ");
+
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                CadastrarProduto();
+                break;
+
+                case 2:
+                FrenteDeCaixa();
+                break;
+
+               case 3:
+               ConsultarEstoque();
+               break;
+
+               case 4:
+               EntradaProdutos();
+               break;
+
+               case 5:
+               ListarProdutos();
+               break;
+
+               case 6:
+               Console.WriteLine("Encerrando...");
+               break;
+
+               default:
+               Console.WriteLine("Opção inválida.");
+               break;
+            }
+
+            Console.WriteLine("\nPressione ENTER para continuar...");
+            Console.ReadLine();
         
+        }while (opcao != 6);
     }
 
-    /**
-    * Procura um produto no vetor de produtos utilizando seu código.
-    *
-    * @param codigoBuscado Código do produto que será pesquisado.
-    * @return A posição do produto no vetor caso ele seja encontrado.
-    *         Retorna -1 caso não exista nenhum produto cadastrado
-    *         com o código informado.
-    */
     static int BuscarProduto(int codigoBuscado)
     {
-        
+        for (int i = 0; i < quantidadeProdutos; i++)
+        {
+            if (produtos[i].Codigo == codigoBuscado)
+            {
+                return i;
+            } 
+        }
+        return -1;
     }
 
-    /*
-    * Responsável por realizar o cadastro de um novo produto no sistema.
-    *
-    * A função deve:
-    * - Verificar se ainda existe espaço disponível para novos produtos;
-    * - Criar um novo produto;
-    * - Gerar automaticamente um código único para o produto;
-    * - Solicitar ao usuário as informações necessárias para o cadastro;
-    * - Armazenar o produto na estrutura de dados utilizada pelo sistema;
-    * - Atualizar a quantidade de produtos cadastrados.
-    *
-    * Caso o limite máximo de produtos seja atingido, o cadastro não deve ser realizado.
-    */
     static void CadastrarProduto()
     {
-        
+        if (quantidadeProdutos >= MAX_PRODUTOS)
+        {
+            Console.WriteLine("Limite Maximo Atingido");
+            return;
+        }
+
+        Produto produto = new Produto();
+
+        produto.Codigo = proximoCodigo++;
+
+        Console.Write("Descrição:");
+        produto.Descricao = Console.ReadLine();
+
+        Console.Write("Preço de Compra:");
+        produto.PrecoCompra = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Preço de Venda:");
+        produto.PrecoVenda = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Quantidade em Estoque:");
+        produto.Estoque = int.Parse(Console.ReadLine());
+
+        produtos[quantidadeProdutos] = produto;
+        quantidadeProdutos++;
+
+        Console.WriteLine("Produto cadastrado com sucesso");
     }
 
-    /*
-    * Responsável por registrar a venda de produtos.
-    *
-    * A função deve:
-    * - Permitir que o usuário informe os códigos dos produtos comprados;
-    * - Encerrar o processo de venda quando o usuário informar o código 0;
-    * - Verificar se o código informado existe no sistema (BuscarProduto);
-    * - Verificar se há quantidade disponível em estoque;
-    * - Exibir os dados dos produtos vendidos;
-    * - Atualizar o estoque após cada venda realizada;
-    * - Calcular o valor total da compra;
-    * - Exibir o total da compra ao final do atendimento.
-    *
-    * Caso um código informado não exista, uma mensagem de erro deve ser exibida.
-    * Caso o produto não possua estoque disponível, a venda não deve ser realizada.
-    */
     static void FrenteDeCaixa()
     {
-        
+        decimal totalCompra = 0;
+        while (true)
+    {
+        Console.Write("Código do produto (0 para finalizar): ");
+        int codigo = int.Parse(Console.ReadLine());
+
+        if (codigo == 0)
+            break;
+
+        int posicao = BuscarProduto(codigo);
+
+        if (posicao == -1)
+        {
+            Console.WriteLine("Código inexistente.");
+            continue;
+        }
+
+        Produto produto = produtos[posicao];
+
+        if (produto.Estoque <= 0)
+        {
+            Console.WriteLine("Produto sem estoque.");
+            continue;
+        }
+
+        produto.Estoque--;
+
+        Console.WriteLine($"Código: {produto.Codigo}");
+        Console.WriteLine($"Descrição: {produto.Descricao}");
+        Console.WriteLine($"Preço de Venda: R$ {produto.PrecoVenda:F2}");
+
+        totalCompra += produto.PrecoVenda;
     }
 
-    /*
-    * Responsável por consultar as informações de um produto
-    * cadastrado no sistema.
-    *
-    * A função deve:
-    * - Solicitar ao usuário o código do produto desejado;
-    * - Verificar se o código informado existe (BuscarProduto);
-    * - Localizar o produto correspondente;
-    * - Exibir todas as informações do produto encontrado:
-    *   - Código;
-    *   - Descrição;
-    *   - Preço de compra;
-    *   - Preço de venda;
-    *   - Quantidade em estoque.
-    *
-    * Caso o código informado não exista, uma mensagem de erro
-    * deve ser exibida ao usuário.
-    */
+    Console.WriteLine($"Valor Total da Compra: R$ {totalCompra:F2}");
+    }
+
+  
     static void ConsultarEstoque()
     {
-        
+        Console.Write("Informe o código do produto: ");
+        int codigo = int.Parse(Console.ReadLine());
+        int posicao = BuscarProduto(codigo);
+
+        if (posicao == -1)
+        {
+            Console.WriteLine("Código inexistente.");
+            return;
+        }
+
+        Produto produto = produtos[posicao];
+
+        Console.WriteLine("=== Dados do Produto ===");
+        Console.WriteLine($"Código: {produto.Codigo}");
+        Console.WriteLine($"Descrição: {produto.Descricao}");
+        Console.WriteLine($"Preço de Compra: R$ {produto.PrecoCompra:F2}");
+        Console.WriteLine($"Preço de Venda: R$ {produto.PrecoVenda:F2}");
+        Console.WriteLine($"Estoque: {produto.Estoque}");
     }
 
-    /*
-    * Responsável por registrar a entrada de novos produtos
-    * no estoque da loja.
-    *
-    * A função deve:
-    * - Solicitar ao usuário o código do produto que será atualizado;
-    * - Verificar se o código informado existe (BuscarProduto);
-    * - Solicitar a quantidade de itens recebidos;
-    * - Solicitar o novo preço de compra do produto;
-    * - Solicitar o novo preço de venda do produto;
-    * - Atualizar a quantidade disponível em estoque;
-    * - Atualizar os preços de compra e venda do produto.
-    *
-    * Caso o código informado não exista, uma mensagem de erro
-    * deve ser exibida ao usuário.
-    */
+
     static void EntradaProdutos()
-    {
-        
+    { 
+        Console.Write("Código do produto: ");
+        int codigo = int.Parse(Console.ReadLine());
+        int posicao = BuscarProduto(codigo);
+        if (posicao == -1)
+        {
+            Console.WriteLine("Código inexistente.");
+            return;
+        }
+
+        Produto produto = produtos[posicao];
+
+        Console.Write("Quantidade adquirida: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        Console.Write("Novo preço de compra: ");
+        produto.PrecoCompra = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Novo preço de venda: ");
+        produto.PrecoVenda = decimal.Parse(Console.ReadLine());
+
+        produto.Estoque += quantidade;
+
+        Console.WriteLine("Produto atualizado com sucesso!");
     }
 
-    /*
-    * Responsável por exibir todos os produtos cadastrados no sistema.
-    *
-    * A função deve:
-    * - Verificar se existe ao menos um produto cadastrado;
-    * - Percorrer a estrutura que armazena os produtos;
-    * - Exibir as informações de cada produto:
-    *   - Código;
-    *   - Descrição;
-    *   - Preço de compra;
-    *   - Preço de venda;
-    *   - Quantidade em estoque.
-    *
-    * Caso não existam produtos cadastrados, uma mensagem
-    * informando essa situação deve ser exibida ao usuário.
-    */
+
     static void ListarProdutos()
     {
-       
+        if (quantidadeProdutos == 0)
+        {
+            Console.WriteLine("Nenhum produto cadastrado.");
+            return;
+        }
+
+        Console.WriteLine("\n=== LISTA DE PRODUTOS ===");
+
+        for (int i = 0; i < quantidadeProdutos; i++)
+        {
+            Produto produto = produtos[i];
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine($"Código: {produto.Codigo}");
+            Console.WriteLine($"Descrição: {produto.Descricao}");
+            Console.WriteLine($"Preço Compra: R$ {produto.PrecoCompra:F2}");
+            Console.WriteLine($"Preço Venda: R$ {produto.PrecoVenda:F2}");
+            Console.WriteLine($"Estoque: {produto.Estoque}");
+        }
     }
 }
